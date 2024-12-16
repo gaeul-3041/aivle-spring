5bfad12ab8e4852de72c69232387476aa0b1dca3
Jin Hyun <pearl5322@gmail.com>
2024-12-16 13:40:09 +0900

    News 컨트롤러 save 기능 추가

--- news/src/main/java/com/example/news/controller/NewsController.java
+++ news/src/main/java/com/example/news/controller/NewsController.java
@@ -1,13 +1,27 @@
 package com.example.news.controller;
 
-
+import com.example.news.domain.News;
+import com.example.news.dto.NewsDto;
+import com.example.news.repository.NewsRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class NewsController {
+    private final NewsRepository newsRepository;
+
     @GetMapping("/news/new")
     public String newArticleForm() {
         return "news/new";
+    }
+
+    @PostMapping
+    public String createNews(NewsDto.Post post) {
+        News news = News.toEntity(post);
+        newsRepository.save(news);
+        return "";
     }
 }
