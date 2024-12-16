f51d6e4cb36ba8aead38608fee8ebd964cbc61f3
Jin Hyun <pearl5322@gmail.com>
2024-12-16 14:06:09 +0900

    mapstruct 적용

--- news/src/main/java/com/example/news/controller/NewsController.java
+++ news/src/main/java/com/example/news/controller/NewsController.java
@@ -2,6 +2,7 @@ package com.example.news.controller;
 
 import com.example.news.domain.News;
 import com.example.news.dto.NewsDto;
+import com.example.news.mapper.NewsMapper;
 import com.example.news.repository.NewsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,14 +14,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class NewsController {
     private final NewsRepository newsRepository;
 
+    private final NewsMapper mapper;  // 의존성 주입(DI)
+
     @GetMapping("/news/new")
     public String newArticleForm() {
         return "news/new";
     }
 
-    @PostMapping
+    @PostMapping("/news/create")
     public String createNews(NewsDto.Post post) {
-        News news = News.toEntity(post);
+        News news = mapper.newsPostDtoToNews(post);
         newsRepository.save(news);
         return "";
     }
