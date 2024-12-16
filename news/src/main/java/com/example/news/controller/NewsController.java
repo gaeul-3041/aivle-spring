3361588afe40640850d2f7568e93e66775d4939e
Jin Hyun <pearl5322@gmail.com>
2024-12-16 14:49:35 +0900

    전체 글 조회 기능 추가 / RequestMapping 설정

--- news/src/main/java/com/example/news/controller/NewsController.java
+++ news/src/main/java/com/example/news/controller/NewsController.java
@@ -11,31 +11,42 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/news")
 public class NewsController {
     private final NewsRepository newsRepository;
 
     private final NewsMapper mapper;  // 의존성 주입(DI)
 
-    @GetMapping("/news/new")
+    @GetMapping("/new")
     public String newArticleForm() {
         return "news/new";
     }
 
-    @PostMapping("/news/create")
+    @PostMapping("/create")
     public String createNews(NewsDto.Post post) {
         News news = mapper.newsPostDtoToNews(post);
         newsRepository.save(news);
-        return "";
+        return "redirect:/news/list";
     }
 
-    @GetMapping("/news/{newsId}")
+    @GetMapping("/{newsId}")
     public String getNews(@PathVariable("newsId") Long newsId, Model model) {
         News news = newsRepository.findById(newsId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));  // Optional 객체의 if-else 간결화
         model.addAttribute("news", news);
         return "news/detail";
+    }
+
+    @GetMapping("/list")
+    public String getNewsList(Model model) {
+        List<News> newsList = newsRepository.findAll();
+        model.addAttribute("newsList", newsList);
+        return "news/list";
     }
 }
