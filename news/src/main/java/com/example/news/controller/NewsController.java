f14293b9a59c0717d8a2958864db8f5753c71e31
Jin Hyun <pearl5322@gmail.com>
2024-12-16 14:42:17 +0900

    뉴스 조회 기능 및 뷰페이지 추가

--- news/src/main/java/com/example/news/controller/NewsController.java
+++ news/src/main/java/com/example/news/controller/NewsController.java
@@ -4,9 +4,12 @@ import com.example.news.domain.News;
 import com.example.news.dto.NewsDto;
 import com.example.news.mapper.NewsMapper;
 import com.example.news.repository.NewsRepository;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -26,5 +29,13 @@ public class NewsController {
         News news = mapper.newsPostDtoToNews(post);
         newsRepository.save(news);
         return "";
+    }
+
+    @GetMapping("/news/{newsId}")
+    public String getNews(@PathVariable("newsId") Long newsId, Model model) {
+        News news = newsRepository.findById(newsId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다."));  // Optional 객체의 if-else 간결화
+        model.addAttribute("news", news);
+        return "news/detail";
     }
 }
