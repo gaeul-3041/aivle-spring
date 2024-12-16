d11521dc021f238f52cc1a0e0f780b7f1c95354a
Jin Hyun <pearl5322@gmail.com>
2024-12-16 15:47:13 +0900

    페이지네이션 - 이전/다음 페이지

--- news/src/main/java/com/example/news/repository/NewsRepository.java
+++ news/src/main/java/com/example/news/repository/NewsRepository.java
@@ -1,7 +1,10 @@
 package com.example.news.repository;
 
 import com.example.news.domain.News;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NewsRepository extends JpaRepository<News, Long> {
+    Page<News> findAll(Pageable pageable);
 }
